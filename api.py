d4a288a335e55199fca8657f225795a45da3f1f0
Joseph Lord <joseph@human-friendly.com>
2013-07-31 02:46:01 +0100

    On the wrong track not sending the crashlog as a file.

--- api.py
+++ api.py
@@ -1,16 +1,37 @@
-from flask import Flask
-from flask.ext import restful
+from flask import Flask, request
+from flask.ext.restful import reqparse, abort, Api, Resource
+from subprocess import Popen, check_output
+from json import dumps
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = Api(app)
+
+symbolicateApp = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/PrivateFrameworks/DTDeviceKit.framework/Versions/A/Resources/symbolicatecrash"
+# export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
 
 
-class HelloWorld(restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Symbolicate(Resource):
+    def post(self):
+#        print request.form
+        crashlog = request.form['crashlog']
+        print crashlog
+        symbolicator = Popen([symbolicateApp, '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        (symbolicated, meta) = symbolicator.communicate(crashlog)
+        print meta
+        print symbolicated
+        return {'symbolicatedCrashlog': symbolicated, 'symbolicatorMetaOutput': meta}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Symbolicate, '/symbolicate')
+
+
+def jsonify_crash_for_testing():
+    inputFileName = "Fast_Lists-2013-07-19-11-46.crash"
+    origFileText = check_output(["cat", inputFileName])
+    origAsDict = {'crashlog': origFileText}
+    jsontext = dumps(origAsDict)
+    print jsontext
+
 
 if __name__ == '__main__':
+   # jsonify_crash_for_testing()
     app.run(debug=True)
-    
